1593f73246a5b0127eb888b1558c52b3650871a5
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-15 16:54:48 +0000

    create

--- monty_pchar.c
+++ monty_pchar.c
@@ -19,7 +19,7 @@ void pchar_init(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	pchar_var = (*stack)->n;
-	if ((pchar_var > 127 + '0') || (pchar_var < 0 + '0'))
+	if ((pchar_var > 127) || (pchar_var < 0))
 	{
 		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 		free(var.buff);
